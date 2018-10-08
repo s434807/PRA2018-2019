ead93ab8b85ae1c69b33f67664150b7206afe3f8
s434807 <bwisniewski887@gmail.com>
2018-10-08 13:12:51 +0200

    echs dee

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -1,8 +1,14 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+
+   static Logger log = Logger.getLogger("jeff");
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+
+        log.info("myname");
     }
 }
